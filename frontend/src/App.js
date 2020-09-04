d18e0139b46550f97e2823dc1b4038b4eea60e9a
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-09-04 19:19:42 +0300

    Add Routes Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./users/pages/Users";
+import NewPlaces from "./places/pages/NewPlaces";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact={true} path="/">
+          <Users />
+        </Route>
+        <Route exact path="/places/new">
+          <NewPlaces />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/places/pages/NewPlaces.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewPlaces() {
+  return <h1>New Places</h1>;
+}
+
+export default NewPlaces;
