b9efbb8743a7b6c43822fe0ef73f3aa8b8c10cf7
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-09-07 13:35:37 +0300

    Add Avatar and Card Component to UserItem Component and The Styles

--- frontend/src/users/components/UserItem.js
+++ frontend/src/users/components/UserItem.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Avatar from "../../shared/components/UIElement/Avatar";
+import Card from "../../shared/components/UIElement/Card";
 
 import "./UserItem.css";
 
@@ -12,17 +16,19 @@ function UserItem({ user: { id, name, image, places } }) {
   }
   return (
     <li className="user-item">
-      <div className="user-list__content">
-        <div className="user-list__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-list__info">
-          <h2>{name}</h2>
-          <h3>
-            {places} {places === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+        <Card className="user-item__content">
+          <Link to={`/${id}/places`}>
+            <div className="user-item__image">
+              <Avatar image={image} alt={name} />
+            </div>
+            <div className="user-item__info">
+              <h2>{name}</h2>
+              <h3>
+                {places} {places === 1 ? "Place" : "Places"}
+              </h3>
+            </div>
+          </Link>
+        </Card>
     </li>
   );
 }
