69b968229c5f6e06d88a366c7c730ae214c49549
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-09-10 21:00:25 +0300

    Add Custom Button

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../../shared/components/UIElement/Card";
+import Button from "../../shared/components/FormElements/Button";
 
 import "./PlaceItem.css";
 
@@ -24,9 +25,9 @@ function PlaceItem({
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,34 +1,39 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 import PlaceList from "../components/PlaceList";
 
+const DUMMY_PLACE = [
+  {
+    id: "p1",
+    title: "Place One",
+    description: "kdnvjfdngkf lgfgbfg knfkbgnfkbn",
+    imageUrl: "kdmckm klvfdmlbkmfgkbmfgblmflbmflnmlkhn",
+    address: "",
+    location: {
+      lat: 45.54545,
+      lng: 32.2545,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Place Tne",
+    description: "kdnvjfdngkf lgfgbfg knfkbgnfkbn",
+    imageUrl: "kdmckm klvfdmlbkmfgkbmfgblmflbmflnmlkhn",
+    address: "",
+    location: {
+      lat: 45.54545,
+      lng: 32.2545,
+    },
+    creator: "u2",
+  },
+];
+
 function UserPlaces() {
-  const DUMMY_PLACE = [
-    {
-      id: "p1",
-      title: "Place One",
-      description: "kdnvjfdngkf lgfgbfg knfkbgnfkbn",
-      imageUrl: "kdmckm klvfdmlbkmfgkbmfgblmflbmflnmlkhn",
-      address: "",
-      location: {
-        lat: 45.54545,
-        lng: 32.2545,
-      },
-      creator: "u1",
-    },
-    {
-      id: "p2",
-      title: "Place Tne",
-      description: "kdnvjfdngkf lgfgbfg knfkbgnfkbn",
-      imageUrl: "kdmckm klvfdmlbkmfgkbmfgblmflbmflnmlkhn",
-      address: "",
-      location: {
-        lat: 45.54545,
-        lng: 32.2545,
-      },
-      creator: "u2",
-    },
-  ];
-  return <PlaceList items={DUMMY_PLACE} />;
+  const userId = useParams().userId;
+  const loadedPlace = DUMMY_PLACE.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlace} />;
 }
 
 export default UserPlaces;
