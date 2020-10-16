17ff45106180bc5338f4fffe64b2e62e81220711
mabadsa <moh22389@gmail.com>
2020-10-16 19:50:15 +0300

    [api] - Handling Error

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,15 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/places', placesRoutes);
 
+app.use((error, req, res, next) => {
+    if(res.headersSent) {
+        return next(error);
+    }
+
+    res.status(error.code || 500);
+    res.json({message: error.message || "An unknown error occurred"})
+})
+
 const port = process.env.PORT || 5500;
 
 app.listen(port, () => {
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,35 +1,53 @@
 const express = require("express");
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-];
+const DUMMY_PLACES = [{
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+        lat: 40.7484474,
+        lng: -73.9871516
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+}];
 
 
 router.route('/').get((req, res, next) => {
-    res.json({ messgae: "Hello" });
+    res.json({
+        messgae: "Hello"
+    });
 })
 
 router.route('/:pid').get((req, res, next) => {
     const placeId = req.params.pid;
-    const place = DUMMY_PLACES.filter(item => item.id === placeId)
-    res.json({ place });
+    const place = DUMMY_PLACES.find(item => item.id === placeId)
+
+    if (!place) {
+        const error = new Error("Could not find a place for the provided id.");
+        error.code = 404;
+        throw error;
+    }
+    console.log(res.headersSent);
+    res.json({
+        place
+    });
 })
 
 router.route('/user/:uid').get((req, res, next) => {
     const userId = req.params.uid;
     const place = DUMMY_PLACES.find(p => p.creator === userId);
-    res.json({ place });
+
+    if (!place) {
+        const error = new Error("Could not find a place for the provided user id.");
+        error.code = 404;
+        return next(error);
+    }
+
+    res.json({
+        place
+    });
 })
 
 module.exports = router;
