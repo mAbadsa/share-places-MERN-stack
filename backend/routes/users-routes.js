c693f29cc1cef71b6e31e8bca0253f76d5f258ea
Muhammad Alabadsa <moh22389@gmail.com>
2020-12-04 21:02:06 +0200

    Add validation to patch place, user and delete place routes Relates #3

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
-const { getUsers, signin, login } = require("../controllers/users-controllers");
+const { getUsers, signup, login } = require("../controllers/users-controllers");
 
 router.route("/").get(getUsers);
 
-router.route("/signup").post(signin);
+router
+  .route("/signup")
+  .post(
+    [
+      check("name").isLength({ min: 5 }),
+      check("email").normalizeEmail().isEmail(),
+      check("password").isLength({ min: 8 }).isAlphanumeric(),
+    ],
+    signup
+  );
 
 router.route("/login").post(login);
 
